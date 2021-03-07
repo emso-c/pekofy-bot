1631d944f07265dae4e0a000736172afe6230304
thennal <thennal10@gmail.com>
2021-03-07 18:12:53 +0530

    deals with other known exceptions
    capitalization, nbsp, and misc.

--- pekofy.py
+++ pekofy.py
@@ -30,8 +30,26 @@ def pekofy(text):
     for match in punctuation_pattern.finditer(text + '\n'):
         i = match.start() + offset # match point
         last_word = regex.search(r'\w+', new_text[i::-1]) # find the nearest alphanumeric behind match point
-        j = i - last_word.start() + 1 # index to insert keyword
-        keyword = jp_keyword if is_japanese(last_word.group()) else en_keyword
+        try:
+            j = i - last_word.start() + 1 # index to insert keyword
+
+            if is_japanese(last_word.group()):
+                keyword = jp_keyword
+            elif last_word.group().isupper():
+                keyword = en_keyword.upper()
+            else:
+                keyword = en_keyword
+        except AttributeError: # If the entire string is just non-alphanumeric
+            continue
+
+        # Exceptions
+        # General case for when there's already a peko and it's newly added
+        already_keyword = (new_text[j - len(keyword):j] == keyword)
+        original_keyword = (text[j - offset - len(keyword):j - offset] == keyword)
+        # nbsp-specific exception
+        nbsp = new_text[j - len('&#x200B'):j] == '&#x200B'
+        if (already_keyword and not original_keyword) or nbsp:
+            continue
 
         new_text = new_text[:j] + keyword + new_text[j:]
         offset += len(keyword)
