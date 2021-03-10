6329f4f2b899a5403d98d97debcd318ed8933d27
emso-c <32599085+emso-c@users.noreply.github.com>
2021-03-10 13:21:51 +0300

    Refactored most of the codebase
    - Reply structure has been redesigned
    - main.py has been rewritten accordingly
    - Added new functions
    - Reply limit has been set to 2 from 3

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import string
 from datetime import datetime
 import time
 import random
-import replies
 import credentials
+import replies as r
 import pekofy as peko  # because it sounds better
 
 bot_name = credentials.bot_name
@@ -17,47 +17,46 @@ reddit = praw.Reddit(client_id=credentials.client_id,
 subreddit_list = ['u_'+bot_name, 'u_'+author, 'hololive', 'VirtualYoutubers', 'Hololewd', 'okbuddyhololive',
                   'goodanimemes', 'VtuberV8', 'Priconne', 'AmeliaWatson', 'GawrGura']
 subreddit = reddit.subreddit('+'.join(subreddit_list))
-keyphrase = '!pekofy'
-keyphrase_delete = '!unpekofy'
+replies = r.replies
 
-
-def reply_f(reply_message, action_type, comment_obj):
+def reply_f(reply, comment_obj, pekofy_msg=None):
     """
     reply to a comment
 
-    :param reply_message: the message that's gonna be sent
-    :type reply_message: string
-    :param action_type: description of the current action for easier debugging
-    :type action_type: string
+    :param reply: the reply type to send
+    :type reply: string
     :param comment_obj: the comment to reply
     :type comment_obj: praw.models.Comment
+    :param pekofy_msg: pekofied message that should only be passed when the reply variable is 'pekofy' to
+    pass the pekofied reply to the replies, defaults to None
+    :type pekofy_msg: string
     """
+    replies["pekofy"]["message"] = pekofy_msg
+    if pekofy_msg and is_triggering(pekofy_msg, "nothing changed"):
+        reply = "nothing changed"
+
+    reply_content = replies[reply]
+    if not random.randint(0, 100) <= (replies[reply]['chance'] if 'chance' in reply_content else 100) or \
+            already_replied_to(comment_obj, reply):
+        return
+
     global comments_replied
     comments_replied += 1
 
-    # if it couldn't be pekofied, give a random pekora clip
-    if reply_message in ["NOTHING_CHANGED", "NO_LETTER"]:
-        reply_message = random.choice(replies.nothing_changed_reply_list)
-
-    # all things set, ready to comment. Last chance to check if the reply is already commented.
-    if already_replied_to(comment_obj, reply_message):
-        return
+    message = random.choice(reply_content["messages"]) if "messages" in replies[reply] else reply_content["message"]
     try:
-        comment_obj.reply(reply_message)
-    except Exception as e:  # unable to comment on the post, potential ban
+        comment_obj.reply(message)
+    except Exception as e:
         comments_replied -= 1
-        print("Couldn't reply: {}".format(e))
-        print('Tried comment: https://www.reddit.com{}'.format(comment_obj.permalink))
-        print('Reply: {}'.format(reply_message))
-        print("------------------------")
-        notify_author(e, comment_obj, reply_message)
-        return
-    print("{}: https://www.reddit.com{}".format(action_type, comment_obj.permalink))
-    print("Message: {}".format(reply_message))
+        print(f"Couldn't reply: {e}")
+        notify_author(e, comment_obj, message)
+    print(f"{reply}: https://www.reddit.com{comment_obj.permalink}")
+    print(f"Reply: {message}")
     print("------------------------")
 
 def already_replied_to(comment, reply):
     """ returns if already replied the same type of comment or not """
+
     comment.refresh()
     comment.replies.replace_more()
     child_comments = comment.replies.list()
@@ -65,84 +64,71 @@ def already_replied_to(comment, reply):
         if top_comment.parent().id != comment.id:
             break
         if top_comment.author == bot_name:
-            if top_comment.body == reply:
-                print("ALREADY REPLIED, CONTINUING...")
-                print("------------------------")
-                return True
-            if top_comment.body in replies.thanks and reply in replies.thanks:
-                print("ALREADY THANKED, CONTINUING...")
-                print("------------------------")
-                return True
-            if top_comment.body in replies.sorrys and reply in replies.sorrys:
-                print("ALREADY SORRIED, CONTINUING...")
-                print("------------------------")
-                return True
-            if top_comment.body == replies.pain_peko_reply:
-                print("ALREADY PAIN PEKO'd, CONTINUING...")
-                print("------------------------")
-                return True
-            if top_comment.body == replies.hey_moona_reply:
-                print("ALREADY HEY MOONA'd, CONTINUING...")
-                print("------------------------")
-                return True
-            if top_comment.body in replies.nothing_changed_reply_list and reply in replies.nothing_changed_reply_list:
-                print("ALREADY NOTHING CHANGED'd, CONTINUING...")
-                print("------------------------")
-                return True
-            if top_comment.body == replies.no_recursion_reply:
-                print("ALREADY NO'd, CONTINUING...")
-                print("------------------------")
-                return True
-            if top_comment.body == replies.limit_reached_reply:
-                print("ALREADY LIMIT REACHED, CONTINUING...")
-                print("------------------------")
-                return True
-            if top_comment.body == replies.bot_score_abuse_reply:
-                print("ALREADY ABUSE PREVENTED, CONTINUING...")
-                print("------------------------")
-                return True
-            if top_comment.body == replies.confused_reply:
-                print("ALREADY CONFUSED, CONTINUING...")  # lol
-                print("------------------------")
-                return True
-            if top_comment.body in replies.insults and reply in replies.insults:
-                print("ALREADY INSULTED, CONTINUING...")
-                print("------------------------")
-                return True
-            if top_comment.body in replies.loves and reply in replies.loves:
-                print("ALREADY LOVED, CONTINUING...")
-                print("------------------------")
-                return True
-            if top_comment.body in replies.cutes and reply in replies.cutes:
-                print("ALREADY CUTE'D, CONTINUING...")
-                print("------------------------")
-                return True
+            if "messages" in replies[reply]:
+                if top_comment.body in replies[reply]["messages"]:
+                    print(f"Already {reply}'d: {top_comment.body} \ncontinuing...")
+                    print("------------------------")
+                    return True
+            else:
+                if top_comment.body == replies[reply]["message"]:
+                    print(f"Already {reply}'d: {top_comment.body} \ncontinuing...")
+                    print("------------------------")
+                    return True
     return False
 
-def notify_author(exception, comment="None", tried_reply="None"):
+def notify_author(exception, comment=None, tried_reply=None):
     """ Notifies to the author, don't forget to whitelist the bot if your PM's are closed """
 
     title = datetime.now().strftime("%Y.%m.%d - %H:%M:%S")
-    if comment != "None" and tried_reply != "None":
-        body = '{} has run into an error: {}\n' \
-               'Here\'s the [link](https://www.reddit.com{}) to the comment.\n' \
-               'Tried to reply this: {}'.format(bot_name, exception, comment.permalink, tried_reply)
+    if comment and tried_reply:
+        body = f"{bot_name} has run into an error: {exception}\n" \
+               f"Here\'s the [link](https://www.reddit.com{comment.permalink}) to the comment.\n" \
+               f"Tried to reply this: {tried_reply}"
     else:
-        body = '{} has run into an error: {}\n'.format(bot_name, exception)
+        body = f'{bot_name} has run into an error: {exception}\n'
     reddit.redditor(author).message(title, body)
 
-def reply_chance(percent):
-    return random.randint(0, 100) <= percent
+def is_triggering(text, reply):
+    """ whether the text triggers the given reply type or not """
+
+    include_bot = replies[reply]['include_bot'] if 'include_bot' in replies[reply] else False
+    if replies[reply]["exact"] if (True if "exact" in replies[reply] else False) else False:
+        condition = [True if trigger == text else False for trigger in replies[reply]["triggers"]]
+    else:
+        condition = [True if trigger in text else False for trigger in replies[reply]["triggers"]]
+    if replies[reply]["trigger_type"] == "all":
+        return all(condition) and ("bot" in text if include_bot else True)
+    return any(condition) and ("bot" in text if include_bot else True)
+
+def passed_limit(comment, limit=2):
+    """ returns true if the same comment has been pekofied too much by
+    climbing up the comment tree until it reaches the limit """
+
+    current_usage = 0
+    for i in range(limit):
+        if comment.parent_id == comment.link_id:
+            break
+        if comment.parent().author == bot_name:
+            comment = comment.parent()
+            if comment.parent_id == comment.link_id:
+                break
+            if comment.parent().author and is_triggering(comment.parent().body, "pekofy"):
+                comment = comment.parent()
+                current_usage += 1
+    return current_usage == limit
+
+def is_top_level(comment):
+    """ returns if the comment is top level (directly replied to the post) """
+    return comment.parent_id == comment.link_id
 
 
-usage_limit = 3  # limiter to prevent spam
-comments_replied = 0
-comments_scanned = 0
+comments_replied, comments_scanned = 0, 0
 
-# time is used for exponential back off in case reddit server is unable to respond
+# used for exponential back off in case reddit server is unable to respond
 initial_wait_time = 10
 current_wait_time = initial_wait_time
 max_wait_time = 600
+reset_limit = 50
 
 while 1:
     try:  # exception handling at it's finest (lol)
@@ -150,124 +136,88 @@ while 1:
         for comment in subreddit.stream.comments():
             comments_scanned += 1
 
-            # comment has been deleted or it's author is the bot itself, move on
+            # comment has been deleted or it's author is the bot itself
             if not comment.author or comment.author == bot_name:
                 continue
 
-            # pain peko (not used regex for faster results)
-            if comment.body.lower() in ["pain", "pain.", "pain...", "pain peko"] and reply_chance(50):
-                reply_f(replies.pain_peko_reply, "Pain peko'd", comment)
+            # pain peko reply
+            reply_f("pain peko", comment) if is_triggering(comment.body.lower(), "pain peko") else None
 
-            # hey moona
-            if "moona" in comment.body.lower() and "pekora" in comment.body.lower() and reply_chance(25):
-                reply_f(replies.hey_moona_reply, "Hey Moona'd", comment)
+            # hey moona reply
+            reply_f("hey moona", comment) if is_triggering(comment.body.lower(), "hey moona") else None
 
             # feedback gratitude
-            if comment.parent_id != comment.link_id:  # not top level comment
-                if comment.parent().author:  # parent comment is not deleted
-                    if comment.parent().author.name == bot_name:  # replying to the bot
-
-                        # positive feedback reply
-                        if ("good" in comment.body.lower() or "best" in comment.body.lower() or "amazing" in comment.body.lower() or "based" in comment.body.lower()) and "bot" in comment.body.lower():
-                            reply_f(random.choice(replies.thanks), "Thanked", comment)
-                        elif "love you" in comment.body.lower():
-                            reply_f(random.choice(replies.loves), "Loved", comment)
-                        elif "cute" in comment.body.lower():
-                            reply_f(random.choice(replies.cutes), "Cute'd", comment)
-
-                        # negative feedback reply
-                        elif "bad" in comment.body.lower() and "bot" in comment.body.lower():
-                            reply_f(random.choice(replies.sorrys), "Sorried", comment)
-                        # others
-                        elif "insult me peko" in comment.body.lower():
-                            reply_f(random.choice(replies.insults), "Insulted", comment)
-
+            if not is_top_level(comment):
+                if comment.parent().author:
+                    if comment.parent().author.name == bot_name:
+                        for feedback in ["thank", "love", "cute", "sorry", "insult"]:
+                            reply_f(feedback, comment) if is_triggering(comment.body.lower(), feedback) else None
 
             # both pekofy and unpekofy written
-            if keyphrase in comment.body and keyphrase_delete in comment.body:
-                reply_f(replies.confused_reply, "Confused", comment)
+            if is_triggering(comment.body, "confused"):
+                reply_f("confused", comment)
                 continue
 
             # if keyword found, try to pekofy
-            if keyphrase in comment.body:
+            if is_triggering(comment.body, "pekofy"):
 
                 # can't pekofy due to comment not having any parent
-                if not comment.parent().author:
+                if not comment.parent().author or not comment.parent().body:
                     continue
 
                 # parent is a post, pekofy accordingly
-                if comment.parent_id == comment.link_id:
-                    if comment.submission.selftext:  # either text or link post
-                        reply_f(peko.pekofy(comment.submission.title + '\n\n' + comment.submission.selftext),
-                                "Pekofied text/link post", comment)
-                    else:  # image post, only pekofy title
-                        reply_f(peko.pekofy(comment.submission.title), "Pekofied post title", comment)
-                    continue
-
-                # don't pekofy if limit already reached.
-                if comment.parent().body == replies.limit_reached_reply and comment.parent().author == bot_name:
+                if is_top_level(comment):
+                    reply_f("pekofy", comment, peko.pekofy(comment.submission.title + '\n\n' + comment.submission.selftext if comment.submission.selftext else comment.submission.title))
                     continue
 
                 # someone tried to break it by recursive calling, kindly say no
-                if keyphrase in comment.parent().body:
-                    reply_f(replies.no_recursion_reply, "No'd", comment)
-                    continue
-
-                # if same sentence has been pekofied too much already, don't pekofy
-                current_usage = 0
-                comment_climb = comment
-                for i in range(usage_limit):
-                    if comment_climb.parent_id == comment_climb.link_id:
-                        break
-                    if comment_climb.parent().author and comment_climb.parent().author == bot_name:
-                        comment_climb = comment_climb.parent()
-                        if comment_climb.parent_id == comment_climb.link_id:
-                            break
-                        if comment_climb.parent().author and keyphrase in comment_climb.parent().body:
-                            comment_climb = comment_climb.parent()
-                            current_usage += 1
-                if current_usage == usage_limit:
-                    reply_f(replies.limit_reached_reply, "Limit reached", comment)
+                if is_triggering(comment.parent().body, "pekofy"):
+                    reply_f("no", comment)
                     continue
 
                 # someone tried to pekofy a good/bad bot reply, don't pekofy
-                if comment.parent().body:
-                    if "good bot" == comment.parent().body.lower() or "good bot." == comment.parent().body.lower() or \
-                            "bad bot" == comment.parent().body.lower() or "bad bot." == comment.parent().body.lower():
-                        reply_f(replies.bot_score_abuse_reply, "Potential abuse prevented", comment)
-                        continue
+                if is_triggering(comment.parent().body.lower(), "bot score abuse"):
+                    reply_f("bot score abuse", comment)
+                    continue
+
+                # don't pekofy if limit already reached before.
+                if comment.parent().body == replies["limit reached"]["message"] and comment.parent().author == bot_name:
+                    continue
+
+                # if the same sentence has been pekofied too much already, don't pekofy
+                if passed_limit(comment):
+                    reply_f("limit reached", comment)
+                    continue
 
                 # try to reply to the comment
-                reply_f(peko.pekofy(comment.parent().body), "Pekofied", comment)
+                reply_f("pekofy", comment, peko.pekofy(comment.parent().body))
 
             # delete keyphrase found
-            if keyphrase_delete in comment.body and comment.parent().author == bot_name and comment.parent().body:
-                print("Unpekofied: ")
-                print('Reply: {}'.format(comment.parent().body))
+            if is_triggering(comment.body, "unpekofy") and comment.parent().author == bot_name and comment.parent().body:
+                print(f'Unpekofied: {comment.parent().body}')
                 comment.parent().delete()
                 print("------------------------")
 
-            # More than [50] comments has been scanned without an incident, reset wait time.
-            if comments_scanned % 50 == 0:
+            # More than [reset_limit] comments has been scanned without an incident, reset wait time.
+            if comments_scanned % reset_limit == 0:
                 current_wait_time = initial_wait_time
     except KeyboardInterrupt:
         print("Keyboard Interrupt. Terminating...")
         break
     except praw.exceptions.RedditAPIException as e:
-        print("RedditAPIException: {}".format(e))
+        print(f"RedditAPIException: {e}")
         notify_author(e)
     except praw.exceptions.PRAWException as e:
-        print("PRAWException: {}".format(e))
+        print(f"PRAWException: {e}")
         notify_author(e)
     except Exception as e:
-        print(e)
+        print(f"Error: {e}")
         notify_author(e)
     finally:
         print("------------------------")
-        print("Replied comments so far: {}".format(comments_replied))
-        print("Scanned comments so far: {}".format(comments_scanned))
-        comments_replied = 0
-        comments_scanned = 0
+        print(f"Replied comments so far: {comments_replied}")
+        print(f"Scanned comments so far: {comments_scanned}")
+        comments_replied, comments_scanned = 0, 0
 
         # not-so-exponential back off
         time.sleep(current_wait_time)
--- replies.py
+++ replies.py
@@ -1,34 +1,111 @@
-pekora_noises = {
-    "naww": "https://www.youtube.com/watch?v=JNgCFHbPARg",
-    "motherf*cker": "https://www.youtube.com/watch?v=1OjQVMiyUMg",
-    "oh no jesus": "https://www.youtube.com/watch?v=MwCNEySMNWg",
-    "yolo": "https://www.youtube.com/watch?v=MSUckSO-Dsw",
-    "ogey rrat": "https://www.youtube.com/watch?v=JacN1MzyeKo",
-    "rrat simulator rrrra": "https://www.youtube.com/watch?v=Xr_pKdyeIJo",
-    "wao wao waoo!":"https://www.youtube.com/watch?v=O9s_HLql2YM",
-    "pardun?":"https://www.youtube.com/watch?v=a3DpRlWdnDw"
+""" STRUCTURE
+replies = {
+    message_type: {
+        message/messages: the message it contains (string/list)
+        triggers: trigger words (list)
+        trigger_type: whether the text should include all the trigger words
+        or just one of them, either 'all' or 'any' (string), defaults to any
+        chance: chance to trigger (int), defaults to 100
+        include_bot: whether the word 'bot' should be searched in the text too or not, defaults to False
+        exact: whether the text should include the *exact* triggers or not, defaults to False
+    },
+    ...
 }
-nothing_changed_reply_list = []
-for title, link in pekora_noises.items():
-    nothing_changed_reply_list.append("Sorry, I couldn't pekofy the comment for some reason peko. "	
-                                      "So here's a video of Pekora saying [{}]({}) instead peko.".format(title, link))
-no_recursion_reply = "[no](https://www.youtube.com/watch?v=3FOe-KNUwb4)"
-pain_peko_reply = "[pain-peko.](https://preview.redd.it/dvk3bft2a9l51.jpg?auto=webp&s=d5e53605dc0e99ed55884fc00c9b965c7dd38e7c)"
-hey_moona_reply = "#Hey Moona!"
-limit_reached_reply = "Sorry peko, but I can't pekofy it any further to prevent spam peko. Thank you for your understanding peko."
-bot_score_abuse_reply = "Sorry peko, can't pekofy that due to potential bot score abuse peko."
-confused_reply = "eh?"
+"""
 
-# some of them are repeated to increase chance
-thanks = ["Thank you peko", "Thank you peko!", "Thank you peko~", "Arigatou peko!", "Thank you peko!",
-          "Thank you peko~", "Arigatou peko!", "Arigatou peko~", "ありがとうぺこ～", "ありがとうぺこ！",
-          "Thank you peko!", "Thank you peko~", "Ehehe", "Ehehe", "Ehehe",
-          "Arigatou peko da ne! [peko~](https://www.youtube.com/watch?v=zOUPxaA6mBM)",
-          "[Arigatou peko.](https://www.youtube.com/watch?v=swGNEJ56EFI)"]
-sorrys = ["Sorry peko ; ;", "Sorry peko...", "G-Gomen peko.", "ごめんぺこ・・・"]
-insults = ["Bakatare ga!", "Bakatare ga!", "Bakatare ga!", "Bakatare ga!", "Anta wa baka nano?", "バカたれが！",
+replies = {
+    "confused": {
+        "message": "eh?",
+        "triggers": ["!pekofy","!unpekofy"],
+        "trigger_type": "all"
+    },
+    "hey moona": {
+        "message": "#Hey Moona!",
+        "chance": 25,
+        "triggers": ["moona", "pekora"],
+        "trigger_type": "all"
+    },
+    "pain peko": {
+        "message": "[pain-peko.](https://preview.redd.it/dvk3bft2a9l51.jpg?auto=webp&s=d5e53605dc0e99ed55884fc00c9b965c7dd38e7c)",
+        "chance": 50,
+        "triggers": ["pain", "pain.", "pain...", "pain peko"],
+        "trigger_type": "any"
+    },
+    "limit reached": {
+        "message": "Sorry peko, but I can't pekofy it any further to prevent spam peko. Thank you for your understanding peko.",
+    },
+    "bot score abuse": {
+        "message": "Sorry peko, can't pekofy that due to potential bot score abuse peko.",
+        "triggers": ["good bot", "good bot!", "good bot.", "bad bot", "bad bot!", "bad bot."],
+        "trigger_type": "any",
+        "exact": True
+    },
+    "no": {
+        "message": "[no](https://www.youtube.com/watch?v=3FOe-KNUwb4)",
+    },
+    "thank": {
+        "messages":
+            ["Thank you peko", "Thank you peko!", "Thank you peko~", "Arigatou peko!", "Thank you peko!",
+            "Thank you peko~", "Arigatou peko!", "Arigatou peko~", "ありがとうぺこ～", "ありがとうぺこ！",
+            "Thank you peko!", "Thank you peko~", "Ehehe", "Ehehe",
+            "Arigatou peko da ne! [peko~](https://www.youtube.com/watch?v=zOUPxaA6mBM)",
+            "[Arigatou peko.](https://www.youtube.com/watch?v=swGNEJ56EFI)"],
+        "triggers": ["good", "best", "amazing", "based"],
+        "trigger_type": "any",
+        "include_bot": True
+    },
+    "sorry": {
+        "messages": ["Sorry peko ; ;", "Sorry peko...", "G-Gomen peko.", "ごめんぺこ・・・"],
+        "triggers": ["bad", "worst", "awful", "terrible"],
+        "trigger_type": "any",
+        "include_bot": True
+    },
+    "insult": {
+        "messages":
+            ["Bakatare ga!", "Bakatare ga!", "Bakatare ga!", "Bakatare ga!", "Anta wa baka nano?", "バカたれが！",
            "ばかたれが！", "あんたはバカなの？", "ぺっ", "Go peko yourself!",
-           "[Disgusting.](https://streamable.com/6ntf2g)"]
-loves = ["Thank you guys. Don't cheat on me, okay? [Peko~](https://streamable.com/8gagri)",
-         "[Love you](https://streamable.com/dbzfxj) too peko!", "I love you too peko!", "Love you too peko~"]
-cutes = ["You're cute too peko!", "You're also cute peko!", "You're cute too peko~", "You're also cute peko~", "Ehehe", "あなたもかわいいぺこ！"]
+           "[Disgusting.](https://streamable.com/6ntf2g)"],
+        "triggers": ["insult me peko"],
+        "trigger_type": "all"
+    },
+    "love":{
+        "messages":
+            ["Thank you guys. Don't cheat on me, okay? [Peko~](https://streamable.com/8gagri)",
+            "[Love you](https://streamable.com/dbzfxj) too peko!", "I love you too peko!", "Love you too peko~"],
+        "triggers": ["love you"],
+        "trigger_type": "all"
+    },
+    "cute": {
+        "messages":
+            ["You're cute too peko!", "You're also cute peko!", "You're cute too peko~", "You're also cute peko~", "Ehehe", "あなたもかわいいぺこ！"],
+        "triggers": ["cute"],
+        "trigger_type": "all",
+    },
+    "nothing changed": {
+        "messages":
+            ["Sorry, I couldn\'t pekofy the comment for some reason peko. So here\'s a video of Pekora saying [{}]({}) instead peko.".format(
+            title, link) for title, link in {
+                "naww": "https://www.youtube.com/watch?v=JNgCFHbPARg",
+                "motherf*cker": "https://www.youtube.com/watch?v=1OjQVMiyUMg",
+                "oh no jesus": "https://www.youtube.com/watch?v=MwCNEySMNWg",
+                "yolo": "https://www.youtube.com/watch?v=MSUckSO-Dsw",
+                "ogey rrat": "https://www.youtube.com/watch?v=JacN1MzyeKo",
+                "rrat simulator rrrra": "https://www.youtube.com/watch?v=Xr_pKdyeIJo",
+                "wao wao waoo!":"https://www.youtube.com/watch?v=O9s_HLql2YM",
+                "pardun?":"https://www.youtube.com/watch?v=a3DpRlWdnDw"
+            }.items()],
+        "triggers": ["NOTHING_CHANGED", "NO_LETTER"],
+        "trigger_type": "any",
+        "exact": True
+    },
+    "pekofy": {
+        "message": None,
+        "triggers": ["!pekofy"],
+        "trigger_type": "all"
+    },
+    "unpekofy": {
+        "message": None,
+        "triggers": ["!unpekofy"],
+        "trigger_type": "all"
+    },
+}
